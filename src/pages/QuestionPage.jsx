0e6d8cd693d068563fb56f47bc65cc23a983874c
Marco Y <marcoysu765@gmail.com>
2026-02-12 23:18:20 -0500

    created json file for music

--- src/pages/QuestionPage.jsx
+++ src/pages/QuestionPage.jsx
@@ -1,12 +1,14 @@
+
+
 function QuestionPage({ setPage }) {
   return (
-    <>
+    <div className="question-page">
       <h1 className="qtn">Will you be my Valentines?</h1>
       <div className="btn-container">
         <button className="btn yes" onClick={setPage}>Yes</button>
         <button className="btn no">No</button>
       </div>
-    </>
+    </div>
   );
 }
 
